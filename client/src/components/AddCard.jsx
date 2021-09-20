9791c7b0ec0859639a60c7e70b8e64c2f1e0c6ce
Briana Gonzalez <brianagonzalez@brianas-air.lan>
2021-09-20 12:49:23 -0500

    create card and form filled out

--- client/src/components/AddCard.jsx
+++ client/src/components/AddCard.jsx
@@ -3,6 +3,15 @@ import { useState } from "react";
 import { useHistory } from "react-router";
 import Form from "./Form"
 
+const airtableBase = process.env.REACT_APP_AIRTABLE_BASE;
+const airtableKey = process.env.REACT_APP_AIRTABLE_KEY;
+const URL = `https://api.airtable.com/v0/${airtableBase}/Binder`;
+const config = {
+  headers: {
+    Authorization: `Bearer ${airtableKey}`,
+  }
+};
+
 function AddCard() {
   const [title, setTitle] = useState("");
   const [name, setName] = useState("");
@@ -12,9 +21,46 @@ function AddCard() {
   const [description, setDescription] = useState("");
   const [image, setImage] = useState("");
   const [set, setSet] = useState("");
+  const history = useHistory();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const fields = {
+      title,
+      name,
+      date,
+      obtained,
+      type,
+      description,
+      image,
+      set,
+    };
+    const res = await axios.post(URL, { fields }, config);
+    history.push("/");
+  }
+
+
   return (
     <div>
-      
+      <Form
+        title={title}
+        setTitle={setTitle}
+        name={name}
+        setName={setName}
+        date={date}
+        setDate={setDate}
+        obtained={obtained}
+        setObtained={setObtained}
+        type={type}
+        setType={setType}
+        description={description}
+        setDescription={setDescription}
+        image={image}
+        setImage={setImage}
+        set={set}
+        setSet={setSet}
+        handleSubmit={handleSubmit}
+      />
     </div>
   )
 }
--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,10 +1,93 @@
 
 
-function Form() {
+function Form(props) {
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={props.handleSubmit}>
+      <input
+        type="text"
+        value={props.title}
+        placeholder="Card Title"
+        onChange={(e) => props.setTitle(e.target.value)}
+      />
+      <br />
+      <input
+        type="text"
+        value={props.name}
+        placeholder="Pokémon Name"
+        onChange={(e) => props.setName(e.target.value)}
+      />
+      <br />
+      <select name="type" id="type" onChange={(e) => props.setType(e.target.value)}>
+        <option value={props.type.grass}>
+          Grass
+        </option>
+        <option value={props.type.fire}>
+          Fire
+        </option>
+        <option value={props.type.water}>
+          Water
+        </option>
+        <option value={props.type.lightning}>
+          Lightning
+        </option>
+        <option value={props.type.psychic}>
+          Psychic
+        </option>
+        <option value={props.type.fighting}>
+          Fighting
+        </option>
+        <option value={props.type.dark}>
+          Dark
+        </option>
+        <option value={props.type.metal}>
+          Metal
+        </option>
+        <option value={props.type.dragon}>
+          Dragon
+        </option>
+        <option value={props.type.fairy}>
+          Fairy
+        </option>
+        <option value={props.type.normal}>
+          Normal
+        </option>
+      </select>
+      <br />
+      <input
+        type="text"
+        value={props.date}
+        placeholder="Date Collected"
+        onChange={(e) => props.setDate(e.target.value)}
+      />
+      <br />
+      <select name="obtained" id="obtained" onChange={(e) => props.setType(e.target.value)}>
+        <option value={props.obtained.gifted}>
+          Gifted Card
+        </option>
+        <option value={props.obtained.pulled}>
+          Pulled Card from a Pack
+        </option>
+        <option value={props.obtained.purchased}>
+          Purchased Card
+        </option>
+      </select>
+      <br />
+      <input
+        type="text"
+        value={props.description}
+        placeholder="Description"
+        onChange={(e) => props.setDescription(e.target.value)}
+      />
+      <br />
+      <input
+        type="text"
+        value={props.image}
+        placeholder="Paste Card Image Address"
+        onChange={(e) => props.setImage(e.target.value)}
+      />
+      <br />
+      <button>Add Card</button>
+    </form>
   )
 }
 
