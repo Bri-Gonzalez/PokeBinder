61652d7caf627317bb2c526be0aa0c7aaa04108c
Briana Gonzalez <brianagonzalez@brianas-air.lan>
2021-09-21 15:45:59 -0500

    saving form CSS progress

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,107 +1,110 @@
-
+import './Form.css';
 
 function Form(props) {
   return (
-    <form onSubmit={props.handleSubmit}>
-      <input
-        type="text"
-        value={props.title}
-        placeholder="Card Title"
-        onChange={(e) => props.setTitle(e.target.value)}
-      />
-      <br />
-      <input
-        type="text"
-        value={props.name}
-        placeholder="Pokémon Name"
-        onChange={(e) => props.setName(e.target.value)}
-      />
-      <br />
-      <input
-        type="text"
-        value={props.set}
-        placeholder="Card Set"
-        onChange={(e) => props.setSet(e.target.value)}
-      />
-      <br />
-      <select name="type" id="type" onChange={(e) => props.setType(e.target.value)}>
-        <option>Select Type</option>
-        <option value={props.type}>
-          Grass
-        </option>
-        <option value={props.type.fire}>
-          Fire
-        </option>
-        <option value={props.type.water}>
-          Water
-        </option>
-        <option value={props.type.lightning}>
-          Lightning
-        </option>
-        <option value={props.type.psychic}>
-          Psychic
-        </option>
-        <option value={props.type.fighting}>
-          Fighting
-        </option>
-        <option value={props.type.dark}>
-          Dark
-        </option>
-        <option value={props.type.metal}>
-          Metal
-        </option>
-        <option value={props.type.dragon}>
-          Dragon
-        </option>
-        <option value={props.type.fairy}>
-          Fairy
-        </option>
-        <option value={props.type.normal}>
-          Normal
-        </option>
-      </select>
-      <br />
-      <input
-        type="text"
-        value={props.date}
-        placeholder="Date Collected"
-        onChange={(e) => props.setDate(e.target.value)}
-      />
-      <br />
-      <select name="obtained" id="obtained" onChange={(e) => props.setObtained(e.target.value)}>
-        <option>
-          How Card Was Aquired
-        </option>
-        <option value={props.obtained.pulled}>
-          Pulled
-        </option>
-        <option value={props.obtained.traded}>
-          Traded
-        </option>
-        <option value={props.obtained.gifted}>
-          Gifted
-        </option>
-        <option value={props.obtained.purchased}>
-          Purchased
-        </option>
-      </select>
-      <br />
-      <input
-        type="text"
-        value={props.description}
-        placeholder="Description"
-        onChange={(e) => props.setDescription(e.target.value)}
-      />
-      <br />
-      <input
-        type="text"
-        value={props.image}
-        placeholder="Paste Card Image Address"
-        onChange={(e) => props.setImage(e.target.value)}
-      />
-      <br />
-      <button>Submit</button>
-    </form>
+    <div className="form-container">
+      <form onSubmit={props.handleSubmit}>
+        <p className="form-title">Card Details</p>
+        <input
+          type="text"
+          value={props.title}
+          placeholder="Card Title"
+          onChange={(e) => props.setTitle(e.target.value)}
+        />
+        <br />
+        <input
+          type="text"
+          value={props.name}
+          placeholder="Pokémon Name"
+          onChange={(e) => props.setName(e.target.value)}
+        />
+        <br />
+        <input
+          type="text"
+          value={props.set}
+          placeholder="Card Set"
+          onChange={(e) => props.setSet(e.target.value)}
+        />
+        <br />
+        <select name="type" id="type" onChange={(e) => props.setType(e.target.value)}>
+          <option>Select Type</option>
+          <option value={props.type}>
+            Grass
+          </option>
+          <option value={props.type.fire}>
+            Fire
+          </option>
+          <option value={props.type.water}>
+            Water
+          </option>
+          <option value={props.type.lightning}>
+            Lightning
+          </option>
+          <option value={props.type.psychic}>
+            Psychic
+          </option>
+          <option value={props.type.fighting}>
+            Fighting
+          </option>
+          <option value={props.type.dark}>
+            Dark
+          </option>
+          <option value={props.type.metal}>
+            Metal
+          </option>
+          <option value={props.type.dragon}>
+            Dragon
+          </option>
+          <option value={props.type.fairy}>
+            Fairy
+          </option>
+          <option value={props.type.normal}>
+            Normal
+          </option>
+        </select>
+        <br />
+        <input
+          type="text"
+          value={props.date}
+          placeholder="Date Collected"
+          onChange={(e) => props.setDate(e.target.value)}
+        />
+        <br />
+        <select name="obtained" id="obtained" onChange={(e) => props.setObtained(e.target.value)}>
+          <option>
+            How Card Was Aquired
+          </option>
+          <option value={props.obtained.pulled}>
+            Pulled
+          </option>
+          <option value={props.obtained.traded}>
+            Traded
+          </option>
+          <option value={props.obtained.gifted}>
+            Gifted
+          </option>
+          <option value={props.obtained.purchased}>
+            Purchased
+          </option>
+        </select>
+        <br />
+        <input
+          type="text"
+          value={props.description}
+          placeholder="Description"
+          onChange={(e) => props.setDescription(e.target.value)}
+        />
+        <br />
+        <input
+          type="text"
+          value={props.image}
+          placeholder="Paste Card Image Address"
+          onChange={(e) => props.setImage(e.target.value)}
+        />
+        <br />
+        <button className="submit-btn">Submit</button>
+      </form>
+    </div>
   )
 }
 
