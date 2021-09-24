0af044b499400e667d378f77b48d18d6f8c077ba
Briana Gonzalez <brianagonzalez@brianas-air.lan>
2021-09-23 21:27:43 -0500

    saving delete confirmation modal

--- client/src/components/DeleteCard.jsx
+++ client/src/components/DeleteCard.jsx
@@ -1,26 +1,73 @@
 import './DeleteCard.css';
+import Modal from "react-modal";
 import { useHistory } from "react-router";
+import { useState } from "react";
 import { deleteButton } from "../services";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "#197092",
+    borderRadius: "10px",
+  },
+};
+
+Modal.setAppElement('#root');
+
+
 function DeleteCard(props) {
+  const [modalIsOpen, setIsOpen] = useState(false);
   const history = useHistory();
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
   
   const handleDelete = async () => {
     await deleteButton(props.id);
     history.push("/");
   }
+
   return (
-    <Link to="/">
-      <FontAwesomeIcon
-        icon={faTrashAlt}
-        onClick={handleDelete}
-        size="lg"
-        className="delete-button"
-      />
-    </Link>
+    <div>
+      {/* <Link to="/"> */}
+        <FontAwesomeIcon
+          icon={faTrashAlt}
+          onClick={openModal}
+          size="lg"
+          className="delete-button"
+        />
+      {/* </Link> */}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+      >
+        <div className="confirm-border">
+          <h3 className="confirm-message">Are you sure you want to delete?</h3>
+          <div className="inline-delete-btns">
+            <div>
+              <button onClick={handleDelete}>Yes</button>
+            </div>
+            <div>
+              <button onClick={closeModal}>No</button>
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </div>
   )
 }
 
