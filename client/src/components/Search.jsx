5e72ca11ab2ea69c8a9b57e242d7a33bb64cedea
Briana Gonzalez <brianagonzalez@brianas-air.lan>
2021-09-28 22:42:52 -0500

    saving transition CSS and search CSS

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,12 +1,57 @@
 import "./Search.css";
-import SearchCard from "./SearchCard";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-function Search() {
+const airtableBase = process.env.REACT_APP_AIRTABLE_BASE;
+const airtableKey = process.env.REACT_APP_AIRTABLE_KEY;
+const URL = `https://api.airtable.com/v0/${airtableBase}/Binder`;
+const config = {
+  headers: {
+    Authorization: `Bearer ${airtableKey}`,
+  }
+};
+
+function SearchCard() {
+  const [card, setCard] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    async function fetchSearch() {
+      const res = await axios.get(URL, config);
+      setCard(res.data.records);
+    }
+    fetchSearch();
+  }, [])
+
   return (
     <div className="search-container">
-      <SearchCard />
+      <form >
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          className="search-input"
+          placeholder="Search by Pokémon"
+        />
+      </form>
+      <div>
+        {card.filter((card) => {
+          if (search === "") {
+            return null;
+          } else if (card.fields.pokemon.toLowerCase().includes(search.toLowerCase())) {
+            return card;
+          } return false;
+        }).map(card => {
+          return (
+            <div key={card.id} className="searched-card-container">
+              <Link to={`/details/${card.id}`} className="searched-card">
+                <img src={card.fields.image} alt={card.fields.card} className="searched-card-image" />
+              </Link>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
 
-export default Search;
+export default SearchCard;
